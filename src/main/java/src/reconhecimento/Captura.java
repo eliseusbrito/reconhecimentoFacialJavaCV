f24654e7e5fc3b4791fe7974a3a70497c4850a90
eliseusbrito <eliseusbrito@gmail.com>
2022-12-20 11:44:35 -0300

    Adicionado detector de face

--- src/main/java/src/reconhecimento/Captura.java
+++ src/main/java/src/reconhecimento/Captura.java
@@ -1,8 +1,14 @@
 package src.reconhecimento;
 
 import org.bytedeco.javacv.*;
+import org.bytedeco.opencv.opencv_core.*;
+import org.bytedeco.opencv.opencv_objdetect.CascadeClassifier;
 
 import java.awt.event.KeyEvent;
+
+import static org.bytedeco.opencv.global.opencv_imgproc.COLOR_BGRA2GRAY;
+import static org.bytedeco.opencv.global.opencv_imgproc.cvtColor;
+import static org.bytedeco.opencv.global.opencv_imgproc.rectangle;
 
 public class Captura {
 
@@ -12,9 +18,22 @@ public class Captura {
         OpenCVFrameGrabber camera = new OpenCVFrameGrabber(0);
         camera.start();
 
+        CascadeClassifier detectorFace = new CascadeClassifier("src\\recursos\\haarcascade_frontalface_alt.xml");
+        //CascadeClassifier detectorFace = new CascadeClassifier("https://raw.github.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_alt.xml");
+
         CanvasFrame cFrame = new CanvasFrame("Preview", CanvasFrame.getDefaultGamma() / camera.getGamma());
         Frame frameCapturado = null;
+        Mat imagemColorida = new Mat();
         while ((frameCapturado = camera.grab()) != null) {
+            imagemColorida = converteMat.convert(frameCapturado);
+            Mat imagemCinza = new Mat();
+            cvtColor(imagemColorida, imagemCinza, COLOR_BGRA2GRAY);
+            RectVector facesDetectadas = new RectVector();
+            detectorFace.detectMultiScale(imagemCinza, facesDetectadas, 1.1, 1, 0, new Size(150, 150), new Size(500, 500));
+            for (int i = 0; i < facesDetectadas.size(); i++) {
+                Rect dadosFace = facesDetectadas.get(0);
+                rectangle(imagemColorida, dadosFace, new Scalar(0, 0, 255, 0));
+            }
             if (cFrame.isVisible()) {
                 cFrame.showImage(frameCapturado);
             }
